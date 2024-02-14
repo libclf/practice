8a624c290a110ba3c26ed1ca19e5aed41fd6ce44
Liling Cheng <libclf1026@gmail.com>
2024-02-15 01:35:04 +0800

    add btn-confirm rwd modal and web collapse, add js for web collapse

--- seats-booking/seat-booking.js
+++ seats-booking/seat-booking.js
@@ -1,2 +1,12 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const displayConfirmWrap = document.querySelector('.confirm-wrap');
+const $btnConfirm = document.querySelector('.action-bar :nth-child(3)');
+const $btnRwdConfirm = document.querySelector('.rwd-fixed-btns :nth-child(2)');
+$btnConfirm.addEventListener('click', btnConfirmOnClicked);
+$btnRwdConfirm.addEventListener('click', btnConfirmOnClicked);
+
+function btnConfirmOnClicked() {
+    displayConfirmWrap.style.display = (displayConfirmWrap.style.display === 'none') ? '' : 'none';
+}
